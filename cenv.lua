dff31f1961dc5cedd52b7e22ac288f9a2f230879
BuildTools <terrariavip0@gmail.com>
2021-12-05 17:17:03 +0300

    Enabling or disabling the copy function

--- cenv.lua
+++ cenv.lua
@@ -58,6 +58,8 @@ getgenv()['cenv'].Find = function(function_name)
         end;
     end;
 end;
+--// Responsible for copying in SG.Generate
+getgenv()['cenv'].Copy = true;
 --// Notify about loading
 printconsole(('Custom functions finished loading, took: %.8f'):format(tick() - Timer),0,127,255);
 printconsole(('Compatibility test is %s'):format(cenv.Test() and 'succeed' or 'failed'),0,218,148);
